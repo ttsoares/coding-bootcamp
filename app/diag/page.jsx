b6fa465569a104fde80fc42d759e8d9ed140449d
Thomas Soares <ttsoares@orion.ufrgs.br>
2023-08-21 16:07:23 -0300

    Improoving resp.

--- app/diag/page.jsx
+++ app/diag/page.jsx
@@ -25,32 +25,38 @@ const Page = () => {
   }
 
   return (
-    <main className="h-full w-full absolute top-0 left-0 border-2  border-yellow-500 md:p-20">
+    <main className="h-full w-full absolute top-0 left-0  md:p-20">
       <div className="flex flex-col md:flex-row w-full h-full relative">
         {/* left - text */}
         <div
-          className="z-10 border-2 border-yellow-400 
+          className="z-10  
         order-last md:order-none
         md:w-2/3 md:h-full "
         >
-          <div className="p-9 md:p-20 border-4 border-red-500 w-full h-2/3 md:mt-24 md:ml-20 text-lg md:text-2xl">
-            <p className="text-center md:text-left">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              repellat iure reiciendis, quas numquam temporibus cupiditate ex
-              rerum amet, libero minus quisquam reprehenderit corporis ab error
-              magnam ullam? Pariatur, sequi?
-            </p>
+          <div className="p-8 md:p-20  w-full h-2/3 text-center md:text-left md:mt-24 md:ml-20 text-lg md:text-2xl">
+            <p className="">{`" ${data[person].text} "`}</p>
+            <div className="flex flex-col md:flex-row mt-6">
+              <p className="font-bold text-lg mr-3">{data[person].name}</p>
+              <p className="text-gray-400 text-lg">{data[person].focus}</p>
+            </div>
           </div>
         </div>
         {/* image */}
         <div
-          className="border-2 border-blue-600 order-first flex flex-col justify-center items-center md:items-start p-6 md:p-0 mt-7 
-          md:mt-0 md:order-none md:absolute md:top-0 md:right-0 md:w-[50%] md:h-full"
+          className=" order-first flex flex-col justify-center items-center md:items-start p-6 first-7 
+          md:p-0 md:mt-0 md:order-none md:absolute md:top-0 md:right-0 md:w-[50%]"
         >
           <div
-            className="bg-slate-500 w-[260px] h-[260px] drop-shadow-xl
-          md:w-[510px] md:h-[510px] md:mt-24 md:ml-24"
-          ></div>
+            className=" w-[260px] h-[260px] drop-shadow-4xl
+          md:w-[540px] md:h-[540px] md:mt-4 md:ml-6"
+          >
+            <Image
+              src={data[person].image}
+              width={550}
+              height={550}
+              alt="photo person"
+            />
+          </div>
 
           <Controls inc={inc} dec={dec} />
         </div>
--- app/layout.jsx
+++ app/layout.jsx
@@ -21,16 +21,16 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} relative w-full h-full`}>
         <div className="flex flex-col md:flex-row w-full h-full">
           <div className="flex flex-col justify-between  h-full w-full md:w-1/2  order-last md:order-first">
-            <div className="w-full flex justify-center md:justify-normal md:w-1/3 md:ml-[345px] md:mt-56 ">
-              <Image src={Quotes} alt="" width={115} height={115} />
+            <div className="w-full flex justify-center md:justify-normal md:w-1/3 md:ml-[345px] mt-8 md:mt-56 ">
+              <Image src={Quotes} alt="" width={80} height={80} />
             </div>
             <div className=" md:w-[80%] h-28 md:h-56 relative items-end">
               <Image src={Curve} fill alt="" />
             </div>
           </div>
 
-          <div className="w-full md:w-1/2 p-6 order-first md:order-none">
-            <Image src={PatternBg} alt="" className="md:mt-20" />
+          <div className="w-full md:w-[50%] md:mr-20 order-first md:order-none">
+            <Image src={PatternBg} alt="" className="md:mt-10" />
           </div>
         </div>
         {children}
